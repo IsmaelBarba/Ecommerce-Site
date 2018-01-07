3da8bd261a3f7de3bd9586c68bba5808bb9cc300
Ismael Barba <ismaelbarba1@gmail.com>
2018-01-07 11:04:40 -0600

    a lot of work

--- config/secret.js
+++ config/secret.js
@@ -2,5 +2,11 @@ module.exports = {
 
   database: 'mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce',
   port: 3000,
-  secretKey: "Arash@$@!#@"
+  secretKey: "ismael"
 }
+
+//mongodb://root:abc123@ds239587.mlab.com:39587/hatshop
+
+//mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce
+
+//Arash@$@!#@
